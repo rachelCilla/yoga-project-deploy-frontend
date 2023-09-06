48427c997e9576c78c781eedd7e1c96045518f4e
Rachel Cilla <104958634+rachelCilla@users.noreply.github.com>
2023-09-06 16:44:43 -0600

    reconfigure db

--- src/components/poses/benefits/ChosenBenefitsPoseList.js
+++ src/components/poses/benefits/ChosenBenefitsPoseList.js
@@ -4,51 +4,50 @@ import PosesCard from "../categories/PosesCard";
 import ErrorBoundary from "../ErrorBoundary";
 import { useState, useEffect } from "react";
 export default function ChosenBenefitsPoseList({
-	handleBackClick,
-	activeItem,
-	mainCategory,
-	activeSubCategory,
+  handleBackClick,
+  activeItem,
+  mainCategory,
+  activeSubCategory,
 }) {
-	const poses =
-		posesBenefitData[activeItem]?.[mainCategory]?.[activeSubCategory] ||
-		[];
-	const [loading, setLoading] = useState(true);
+  const poses =
+    posesBenefitData[activeItem]?.[mainCategory]?.[activeSubCategory] || [];
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		if (poses.length > 0) {
-			setLoading(false);
-		}
-	}, [poses]);
+  useEffect(() => {
+    if (poses.length > 0) {
+      setLoading(false);
+    }
+  }, [poses]);
 
-	return (
-		<div className="m-2   pt-3 rounded">
-			<div className="bg-light mb-2 rounded p-2">
-				<h3 className="m-1 mb-2">
-					List of poses that
-					<span className="block underline ">
-						{activeSubCategory}{" "}
-					</span>{" "}
-				</h3>
-				<button
-					className="mb-2 mt-2 px-5 btn "
-					style={{
-						backgroundColor: "#7E6765",
-						color: "#FFFFFF",
-					}}
-					onClick={handleBackClick}
-				>
-					Back
-				</button>
-			</div>
-			{loading && <div className="text-center">Loading...</div>}
-			{poses.map((pose, index) => (
-				<div key={index}>
-					<ErrorBoundary>
-						<PosesCard pose={pose} />
-					</ErrorBoundary>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="m-2   pt-3 rounded">
+      <div className="bg-light mb-2 rounded p-2">
+        <h3 className="m-1 mb-2">
+          List of poses that
+          <span className="block underline ">{activeSubCategory} </span>{" "}
+        </h3>
+        <button
+          className="mb-2 mt-2 px-5 btn "
+          style={{
+            backgroundColor: "#7E6765",
+            color: "#FFFFFF",
+          }}
+          onClick={handleBackClick}
+        >
+          Back
+        </button>
+      </div>
+      {loading && (
+        <div className=" text-xl text-white text-center">Loading...</div>
+      )}
+      {poses.map((pose, index) => (
+        <div key={index}>
+          <ErrorBoundary>
+            <PosesCard pose={pose} />
+          </ErrorBoundary>
+        </div>
+      ))}
+    </div>
+  );
 }
 // poses benefits page: (13) ['Cat', 'Dolphin', 'King Pigeon', 'Half Boat', 'Half-Moon', 'Handstand', 'Plow', 'Seated Forward Bend', 'Standing Forward Bend', 'Lotus', 'Shoulder Stand', 'Side Plank', 'Triangle']
--- src/components/poses/categories/ChosenCategoryPoseList.js
+++ src/components/poses/categories/ChosenCategoryPoseList.js
@@ -7,91 +7,82 @@ import { useOutletContext } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function ChosenCategoryPoseList() {
-	const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-	// const categoryName = useParams();
-	const { category_name: categoryName } = useParams();
-	const { categories } = useOutletContext();
-	// Inside the component, add a check for categories existence
-	if (!categories || categories.length === 0) {
-		return (
-			<div className="text-2xl">
-				Loading... This may take a moment.
-			</div>
-		);
-	}
-	// console.log("categories:", categories);
-	// console.log("categoryName:", categoryName);
+  // const categoryName = useParams();
+  const { category_name: categoryName } = useParams();
+  const { categories } = useOutletContext();
+  // Inside the component, add a check for categories existence
+  if (!categories || categories.length === 0) {
+    return <div className="text-2xl">Loading... This may take a moment.</div>;
+  }
+  // console.log("categories:", categories);
+  // console.log("categoryName:", categoryName);
 
-	// useEffect(() => {
-	// 	if (categoryName && categories) {
-	// 		setLoading(false);
-	// 		console.log("categoryName:", categoryName);
-	// 	}
-	// }, [categoryName, categories]);
+  // useEffect(() => {
+  // 	if (categoryName && categories) {
+  // 		setLoading(false);
+  // 		console.log("categoryName:", categoryName);
+  // 	}
+  // }, [categoryName, categories]);
 
-	// if (loading) {
-	// 	return <div>Loading...</div>;
-	// }
+  // if (loading) {
+  // 	return <div>Loading...</div>;
+  // }
 
-	// Find the object where category_name matches categoryName
-	const selectedCategory = categories.find(
-		(category) => category.category_name === categoryName
-	);
-	const poses = selectedCategory.poses;
+  // Find the object where category_name matches categoryName
+  const selectedCategory = categories.find(
+    (category) => category.category_name === categoryName
+  );
+  const poses = selectedCategory.poses;
 
-	// Now selectedCategory contains the object where the category_name matches categoryName
-	// console.log(selectedCategory);
+  // Now selectedCategory contains the object where the category_name matches categoryName
+  // console.log(selectedCategory);
 
-	return (
-		<div className=" pt-24 bg-grayBlueDarker">
-			<div className="text-center">
-				<h1 className="display-4 font-mont font-semibold text-center text-white">
-					{categoryName}
-				</h1>
-				<Link
-					to="/posecategorycard"
-					className="mb-3  px-5 py-2 btn "
-					style={{
-						backgroundColor: "#7E6765",
-						color: "#FFFFFF",
-					}}
-				>
-					Back to Categories
-				</Link>
-			</div>
+  return (
+    <div className=" pt-24 bg-grayBlueDarker">
+      <div className="text-center">
+        <h1 className="display-4 font-mont font-semibold text-center text-white">
+          {categoryName}
+        </h1>
+        <Link
+          to="/posecategorycard"
+          className="mb-3  px-5 py-2 btn "
+          style={{
+            backgroundColor: "#7E6765",
+            color: "#FFFFFF",
+          }}
+        >
+          Back to Categories
+        </Link>
+      </div>
 
-			<div className="row">
-				<div className=" d-flex flex-wrap flex-fill">
-					{poses.map((pose) => (
-						<div
-							className="col-12 col-md-6 col-lg-4 mb-4  "
-							key={pose.id}
-						>
-							<div className="d-flex flex-column h-100">
-								<PosesCard
-									categoriesPose={pose}
-								/>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-			<div
-				className="mx-auto d-flex justify-content-center
+      <div className="row">
+        <div className=" d-flex flex-wrap flex-fill">
+          {poses.map((pose) => (
+            <div className="col-12 col-md-6 col-lg-4 mb-4  " key={pose.id}>
+              <div className="d-flex flex-column h-100">
+                <PosesCard categoriesPose={pose} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        className="mx-auto d-flex justify-content-center
 			"
-			>
-				<Link
-					to="/posecategorycard"
-					className="mb-3  px-5 py-2 btn "
-					style={{
-						backgroundColor: "#7E6765",
-						color: "#FFFFFF",
-					}}
-				>
-					Back
-				</Link>
-			</div>
-		</div>
-	);
+      >
+        <Link
+          to="/posecategorycard"
+          className="mb-3  px-5 py-2 btn "
+          style={{
+            backgroundColor: "#7E6765",
+            color: "#FFFFFF",
+          }}
+        >
+          Back
+        </Link>
+      </div>
+    </div>
+  );
 }
