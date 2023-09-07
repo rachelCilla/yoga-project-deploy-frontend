17ffa67729c6af3b2f446cf2e0dbd783c7df5142
Rachel Cilla <104958634+rachelCilla@users.noreply.github.com>
2023-09-07 11:31:03 -0600

    fix favorite poses data

--- src/components/FavoritePosesList.js
+++ src/components/FavoritePosesList.js
@@ -1,104 +1,132 @@
 import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import PosesCard from "./poses/categories/PosesCard";
+import PosesCard from "./FavoritesPosesCard";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
-export default function FavoritePosesList({ handleBackButtonClick, favoritePose, showFavorites }) {
-	const [cookies, setCookie, removeCookie] = useCookies(null);
-	const [apiData, setApiData] = useState(null);
-	const [showingFavorites, setShowingFavorites] = useState(true);
+export default function FavoritePosesList({
+  handleBackButtonClick,
+  favoritePose,
+  showFavorites,
+}) {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const [apiData, setApiData] = useState(null);
+  const [showingFavorites, setShowingFavorites] = useState(true);
 
-	const userEmail = cookies.Email;
-	const location = useLocation();
-	const loggedIn = location.state;
+  const userEmail = cookies.Email;
+  const location = useLocation();
+  const loggedIn = location.state;
 
-	const getPoseData = (pose) => {
-		return axios
-			.get(`https://yoga-api-nzy4.onrender.com/v1/poses?name=${pose}`)
-			.then((response) => {
-				return response.data;
-			})
-			.catch((error) => {
-				console.log(error);
-				return null;
-			});
-	};
+  const getPoseData = async (pose) => {
+    return axios
+      .get(`https://yoga-api-nzy4.onrender.com/v1/poses?name=${pose}`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        return null;
+      });
+  };
 
-	const getAllFavorites = async () => {
-		try {
-			const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/favorite_poses/${userEmail}`, {
-				method: "GET",
-				headers: { "Content-Type": "application/json" },
-			});
-			const data = await response.json();
-			return data;
-			// const dataNames = data.map((obj) => obj.pose_name);
-			// const uniqueDataNames = [...new Set(dataNames)];
-			//     //  uniqueDataNames = ['Boat', 'Half Boat', 'Chair', 'Crow', 'Dolphin', 'Side Plank', 'Plank']
-			// const fetchAllPoseData = async () => {
-			//         await Promise.all(uniqueDataNames.map(pose => getPoseData(pose)));
+  const getAllFavorites = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/favorite_poses/${userEmail}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const data = await response.json();
 
-			// };
-		} catch (err) {
-			console.log(err);
-		}
-	};
+      //   console.log(data)
+      //   {id: '704b8964-c577-476c-ba67-ff499fce0263', user_email: 'a', pose_name: 'Extended Side Angle', date: '2023-09-07T17:04:09.275Z'}
 
-	// IMMEDIATELY CALLED
-	useEffect(() => {
-		getAllFavorites()
-			.then((data) => {
-				const dataNames = data.map((obj) => obj.pose_name);
-				const uniqueDataNames = [...new Set(dataNames)];
+      return data;
 
-				const fetchAllPoseData = async () => {
-					// console.log('unique', uniqueDataNames)=['Boat', 'Half Boat', 'Chair', 'Crow', 'Dolphin', 'Side Plank', 'Plank']
-					const poseData = await Promise.all(uniqueDataNames.map((poseName) => getPoseData(poseName)));
-					//    console.log('posedata',poseData)
-					// ;=(7) [undefined, undefined, undefined, undefined, undefined, undefined, undefined]
-					const filteredPoseData = poseData.filter((pose) => pose !== null);
-					setApiData(filteredPoseData);
-				};
+      // const dataNames = data.map((obj) => obj.pose_name);
+      // const uniqueDataNames = [...new Set(dataNames)];
+      //     //  uniqueDataNames = ['Boat', 'Half Boat', 'Chair', 'Crow', 'Dolphin', 'Side Plank', 'Plank']
+      // const fetchAllPoseData = async () => {
+      //         await Promise.all(uniqueDataNames.map(pose => getPoseData(pose)));
 
-				fetchAllPoseData();
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
+      // };
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	return (
-		<div className="bg-grayBlueDarker w-screen h-screen">
-			<h1 className=" text-5xl text-white font-mont p-4 mt-20 text-center ">Favorite Poses List</h1>
-			{loggedIn && (
-				<>
-					<h3> {userEmail}</h3>
-					{apiData !== null &&
-						apiData.map((poseObj) => (
-							<div key={poseObj.id}>
-								<PosesCard
-									pose={poseObj}
-									showingFavorites={showingFavorites}
-								/>
-							</div>
-						))}
-				</>
-			)}
-			{!loggedIn && (
-				<div className="flex flex-col">
-					<h3 className="text-xl text-white font-mont  p-3  ">
-						{" "}
-						To view your favorite poses, please log in or create an account to start saving your favorites.
-					</h3>
-					<Link
-						className="mx-auto"
-						to="/auth">
-						<button className="btn btn-secondary">Login or Sign Up</button>
-					</Link>
-				</div>
-			)}
-		</div>
-	);
+  // IMMEDIATELY CALLED
+  useEffect(() => {
+    getAllFavorites()
+      .then((data) => {
+        const dataNames = data.map((obj) => obj.pose_name);
+
+        const uniqueDataNames = [...new Set(dataNames)];
+
+        const fetchAllPoseData = async () => {
+          // console.log('unique', uniqueDataNames)=['Boat', 'Half Boat', 'Chair', 'Crow', 'Dolphin', 'Side Plank', 'Plank']
+          const poseData = await Promise.all(
+            uniqueDataNames.map((poseName) => getPoseData(poseName))
+          );
+          //           console.log("posedata", poseData);
+
+          // {id: 18, english_name: 'Extended Side Angle', sanskrit_name_adapted: 'Utthita Parsvakonasana', sanskrit_name: '
+          const filteredPoseData = poseData.filter((pose) => pose !== null);
+          setApiData(filteredPoseData);
+          //   console.log(apiData)
+        };
+
+        fetchAllPoseData();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="bg-grayBlueDarker  w-screen h-screen px-10">
+      <h1 className=" text-5xl text-white font-mont p-4 mt-20 text-center ">
+        Favorite Poses List
+      </h1>
+
+      {loggedIn && (
+        <>
+          <h3 className="text-white text-xl lg:text-2xl text-center text-primary font-mont">
+            {" "}
+            User: {userEmail}
+          </h3>
+          {apiData !== null &&
+            apiData.map((poseObj) => (
+              <div style={{ margin: "3rem" }} key={poseObj.id}>
+                <PosesCard
+                  selectedPose={poseObj}
+                  showingFavorites={showingFavorites}
+                />
+              </div>
+            ))}
+          {apiData.length === 0 && (
+            <div className="text-white text-2xl text-center">
+              You currently have no favorites
+            </div>
+          )}
+        </>
+      )}
+
+      {!loggedIn && (
+        <div className="flex flex-col">
+          <h3 className="text-xl text-white font-mont  p-3  ">
+            {" "}
+            To view your favorite poses, please log in or create an account to
+            start saving your favorites.
+          </h3>
+          <Link className="mx-auto" to="/auth">
+            <button className="btn btn-secondary">Login or Sign Up</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
